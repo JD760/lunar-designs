756e68b61a1ee916a84335a4c98b1b4184129cc8
JD760 <jacobdicken2005@gmail.com>
2022-06-30 12:05:16 +0100

    Added session handling

--- server/index.ts
+++ server/index.ts
@@ -1,13 +1,15 @@
-const express = require("express");
+import express from "express";
+import dotenv from "dotenv";
 
+dotenv.config();
 const PORT = process.env.PORT || 3001;
-
 const app = express();
+
 
 app.get("/api", (req, res) => {
     res.json({message: "Hello World!"});
 });
 
 app.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`);
+    console.log(`Server listening on ${PORT}`);
 });
