c30226f050d50868f757596c5c816b7711c11a64
JD760 <jacobdicken2005@gmail.com>
2022-07-17 23:59:21 +0100

    Started selector component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -31,7 +31,7 @@ function App() {
 
     return (
         <div className="App">
-            <AdminLoginComponent />
+            <MainPageComponent />
         </div>
     );
 }
---
+++ client/src/components/SelectorComponent.tsx
@@ -0,0 +1,11 @@
+type SelectorComponentProps = {
+
+}
+
+function SelectorComponent(props: SelectorComponentProps): JSX.Element {
+    return(<div>
+        <p>Hello World!</p>
+    </div>)
+}
+
+export default SelectorComponent;
